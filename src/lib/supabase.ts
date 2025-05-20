96c63d3de6ae766e58f3a02f3b1251e73d52e6e9
dinhhieu110 <trandinhhieu11002@gmail.com>
2025-05-20 22:27:57 +0700

    [ST-00] Setup DB with Supabase

---
+++ src/lib/supabase.ts
@@ -0,0 +1,10 @@
+import { createClient } from "@supabase/supabase-js";
+const supabaseUrl = process.env.VITE_SUPABASE_URL;
+const supabaseKey = process.env.VITE_SUPABASE_KEY;
+if (!supabaseUrl || !supabaseKey)
+  throw new Error(
+    "Supabase key or url is missing. Please check your environment variables."
+  );
+const supabase = createClient(supabaseUrl, supabaseKey);
+
+export default supabase;
