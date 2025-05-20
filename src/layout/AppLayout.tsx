8954f4527bb487901a41f48b5f09906755266e3f
dinhhieu110 <trandinhhieu11002@gmail.com>
2025-05-20 23:52:37 +0700

    [ST-01] Build responsive header

--- src/layout/AppLayout.tsx
+++ src/layout/AppLayout.tsx
@@ -1,14 +1,19 @@
+import { Header } from "@/components/custom";
 import { Outlet } from "react-router-dom";
 
 const AppLayout = () => {
   return (
     <div>
-      <main>
+      <main className="min-h-screen px-4 sm:px-8 lg:px-12 xl:px-16">
         {/* Header */}
+        <Header />
         {/* Body */}
         <Outlet />
-        {/* Footer */}
       </main>
+      {/* Footer */}
+      <div className="p-10 text-center bg-gray-800 mt-10">
+        Tran Dinh Hieu @Wilson_Tran
+      </div>
     </div>
   );
 };
