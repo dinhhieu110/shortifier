01e942c728ab5733a87f66f603b31a59c226b212
dinhhieu110 <trandinhhieu11002@gmail.com>
2025-05-21 10:26:59 +0700

    [ST-00] Update Header & Footer

---
+++ src/components/Footer.tsx
@@ -0,0 +1,10 @@
+const Footer = () => {
+  return (
+    <div className="p-10 text-center bg-[#333] mt-10 text-white">
+      © 2025 ShortUrl.at - Tool to shorten a long link <br />
+      Tran Dinh Hieu
+    </div>
+  );
+};
+
+export default Footer;
